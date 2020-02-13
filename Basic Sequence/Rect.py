4eaff73e4303d254917bb53de87e6345ba2b3ab8
crisun <1980486919@qq.com>
2020-02-13 18:27:51 +0800

    add file

---
+++ Basic Sequence/Rect.py
@@ -0,0 +1,18 @@
+#矩形序列
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+n0 = 5
+n = np.arange(-5,10)
+y = np.zeros(np.size(n))
+y[np.where((n>=0)&(n<n0))] = 1
+
+plt.stem(n,y)
+plt.xlabel('n')
+plt.xticks(np.arange(-5,10))
+plt.ylabel('y')
+plt.yticks([0,1])
+plt.title(f'$R_{n0}(n)$')#f-String only for Python3.6+
+
+plt.show()
