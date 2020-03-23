f1adc9d275b90dee9cc157073f200fbdec93dd46
crisun <1980486919@qq.com>
2020-03-23 21:48:45 +0800

    modify file

--- FFT/code/myfft.py
+++ FFT/code/myfft.py
@@ -1,10 +1,74 @@
+import sys
+import math
 import numpy as np
 
 def myfft(xn, N):
-	if N & (N-1) != 0 or N < len(xn):
-		print('Syntax Error!')
-		print('Function: myfft(xn, N)')
-		print('   Usage: 1.N must be the power of 2')
-		print('          2.N must be greater or equal to the length of xn')
-		return ;
-myfft([1, 2, 3], 3)
+    
+    if N & (N - 1) != 0 or N < len(xn):
+        print('Syntax Error!')
+        print('Function: myfft(xn, N)')
+        print('   Usage: 1.N must be the power of 2')
+        print('          2.N must be greater or equal to the length of xn')
+        return ;
+
+    #add zero to the end of xn
+    xn = np.append(xn, np.zeros(N - len(xn), dtype = int))
+    
+    #reverse order input
+    Xk = np.array([], dtype = complex)
+    for i in range(0, N):
+        a = bin2dec(dec2bin(i,N)[::-1])
+        Xk = np.append(Xk, xn[a])
+    
+    #main loop
+    levels = int(math.log2(N))          #the num of level
+    NumOfBS = int(N/2)                  #BS in each level
+
+    for level in range(1, levels + 1):
+        NumOfGroup = int(N/(2**level))
+        GroupInterval = 2**level
+        BSInterval = 2**(level - 1)
+        Wn = np.exp(-1j*2*math.pi/GroupInterval)
+
+        for g in range(NumOfGroup):
+            start1 = g * GroupInterval
+            start2 = g * GroupInterval + BSInterval
+
+            for b in range(BSInterval):
+                Xk[b+start1] = Xk[b+start1] + Xk[b+start2]*(Wn**b)
+                Xk[b+start2] = Xk[b+start1] - Xk[b+start2]*(Wn**b)*2
+    return Xk
+
+def myifft(Xk, N):
+   return  np.conj(myfft(np.conj(Xk),N))/N
+
+def nextpow2(N):
+    return  2**(math.ceil(np.log2(N)))
+
+def bin2dec(num):
+    N = len(num)
+    res = 0
+    j = 0
+    for i in num:
+        res = res + int(i) * 2**(N-1-j)
+        j += 1
+    return res
+
+def dec2bin(num, N):
+    res = []
+    for i in range(int(math.log2(N))):
+        num, rem = divmod(num, 2)
+        res.append(rem)
+    return ''.join([str(x) for x in res[::-1]])
+
+if __name__ == '__main__':
+    a = np.array([1, 2, 3, 2, 1])	
+    print(a)
+    if len(sys.argv) == 1:
+        N = nextpow2(len(a))
+    else:
+        N = eval(sys.argv[1])
+    res =  myfft(a, N)
+    print(res)
+    ires = myifft(res, N)
+    print(ires)
